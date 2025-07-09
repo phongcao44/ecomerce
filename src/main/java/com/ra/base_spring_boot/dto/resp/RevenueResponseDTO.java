5c2bd041de2588acb0aa57206b9c764c52470ca9
NhutLe253 <nhutlmce@gmail.com>
2025-07-09 17:19:11 +0700

    fix revenue

--- src/main/java/com/ra/base_spring_boot/dto/resp/RevenueResponseDTO.java
+++ src/main/java/com/ra/base_spring_boot/dto/resp/RevenueResponseDTO.java
@@ -1,11 +1,14 @@
 package com.ra.base_spring_boot.dto.resp;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
 @Setter
 @AllArgsConstructor
@@ -15,4 +18,10 @@ public class RevenueResponseDTO {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate time;         // ngày / tháng / năm
     private BigDecimal totalRevenue;
+    private Integer totalOrders;
+    private Integer totalCustomers;
+    private Integer totalProducts;
+    private Integer totalDelivered;
+    private List<Top5Product> top5BestSellers; // Thêm danh sách sản phẩm bán chạy nhất
+
 }
--- src/main/java/com/ra/base_spring_boot/services/impl/RevenueServeImpl.java
+++ src/main/java/com/ra/base_spring_boot/services/impl/RevenueServeImpl.java
@@ -1,10 +1,15 @@
 package com.ra.base_spring_boot.services.impl;
 
 import com.ra.base_spring_boot.dto.resp.RevenueResponseDTO;
+import com.ra.base_spring_boot.dto.resp.Top5Product;
 import com.ra.base_spring_boot.model.Order;
 import com.ra.base_spring_boot.model.constants.OrderStatus;
+import com.ra.base_spring_boot.model.constants.RoleName;
 import com.ra.base_spring_boot.repository.IOrderRepository;
+import com.ra.base_spring_boot.repository.IProductRepository;
+import com.ra.base_spring_boot.repository.IUserRepository;
 import com.ra.base_spring_boot.services.IOrderService;
+import com.ra.base_spring_boot.services.IProductService;
 import com.ra.base_spring_boot.services.IRevenueService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,8 +26,14 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class RevenueServeImpl implements IRevenueService {
 
-    @Autowired
-    private IOrderRepository orderRepository;
+    private final IOrderRepository orderRepository;
+
+    private final IUserRepository userRepository;
+
+    private final IProductRepository productRepository;
+
+    private final IProductService productService;
+
 
     @Override
     public List<RevenueResponseDTO> getRevenueByRange(LocalDate from, LocalDate to) {
@@ -39,9 +50,40 @@ public class RevenueServeImpl implements IRevenueService {
                 ));
 
         return grouped.entrySet().stream()
-                .map(entry -> new RevenueResponseDTO(entry.getKey(), entry.getValue()))
+                .map(entry -> RevenueResponseDTO.builder()
+                        .time(entry.getKey())
+                        .totalRevenue(entry.getValue())
+                        .build())
                 .sorted(Comparator.comparing(RevenueResponseDTO::getTime))
                 .collect(Collectors.toList());
+
+    }
+
+    @Override
+    public RevenueResponseDTO getDashboardStats() {
+        List<Order> deliveredOrders = orderRepository.findAllByStatus(OrderStatus.DELIVERED);
+
+        BigDecimal totalRevenue = deliveredOrders.stream()
+                .map(Order::getTotalAmount)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        int totalOrders = orderRepository.findAll().size();
+        int totalCustomers = (int) userRepository.findAll().stream()
+                .filter(user -> user.getRoles().stream()
+                        .anyMatch(role -> role.getName() == RoleName.ROLE_USER))
+                .count();
+        int totalProducts = productRepository.findAll().size();
+        int totalDelivered = deliveredOrders.size();
+        List<Top5Product> top5bestseller = productService.getTop5BestSellingProducts();
+
+        return RevenueResponseDTO.builder()
+                .totalRevenue(totalRevenue)
+                .totalOrders(totalOrders)
+                .totalCustomers(totalCustomers)
+                .totalProducts(totalProducts)
+                .totalDelivered(totalDelivered)
+                .top5BestSellers(top5bestseller)
+                .build();
     }
 
     @Override
@@ -75,8 +117,12 @@ public class RevenueServeImpl implements IRevenueService {
                 ));
 
         return grouped.entrySet().stream()
-                .map(entry -> new RevenueResponseDTO(entry.getKey(), entry.getValue()))
+                .map(entry -> RevenueResponseDTO.builder()
+                        .time(entry.getKey())
+                        .totalRevenue(entry.getValue())
+                        .build())
                 .sorted(Comparator.comparing(RevenueResponseDTO::getTime))
                 .collect(Collectors.toList());
+
     }
 }
